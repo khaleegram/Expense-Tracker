e8bb68fc06109e8c74312d1f9dc9dae8aaf735dd
Muhammad Abdulrahman <stillkhaleefah@gmail.com>
2025-09-06 18:19:09 +0000

    NEXT_PUBLIC_FIREBASE_API_KEY=AIzaSyDcgWkzjEcOGBXszj04dXqVmabuQuhqU_k NEX

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDcgWkzjEcOGBXszj04dXqVmabuQuhqU_k",
+  authDomain: "expense-a787a.firebaseapp.com",
+  projectId: "expense-a787a",
+  storageBucket: "expense-a787a.firebasestorage.app",
+  messagingSenderId: "1088585223070",
+  appId: "1:1088585223070:web:242206029911bd0a1b648f",
+  measurementId: "G-7RSWBK0DPX"
 };
 
 // Initialize Firebase
